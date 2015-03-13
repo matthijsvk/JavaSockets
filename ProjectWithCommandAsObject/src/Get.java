2cadb51a9dbdcef471f2349a3795a16d62cc72f7
GertJanEnHarald <spamport@hotmail.com>
2015-03-13 09:48:26 +0100

    Ik parse nu de header voor extensions te krijgen, content length is analoog. Dat zal ik ook doen. Want HTTP . sluit de connectie niet. Dus dan moet je zelfs weten wanneer de stream gedaan is.

--- ProjectWithCommandAsObject/src/Get.java
+++ ProjectWithCommandAsObject/src/Get.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 
 public class Get extends RetrieveDataCommand {
+	
+protected String extension;
 
 	
 public Get(String shortHost,String hostExtension,String HTTPVersion, String command, DataOutputStream outToServer, BufferedInputStream inFromServer) {
@@ -17,8 +19,29 @@ public void execute() throws IOException{
 
 super.execute();
 
+
+int indexOfType = header.indexOf("Content-Type:");
+boolean extensionNotFound = true;
+while (extensionNotFound ){
+	if( (header.substring(indexOfType,indexOfType+1)).equals("/")){
+	extensionNotFound = false;
+	}
+	indexOfType += 1;
+}
+boolean endOfExtensionNotFound = true;
+int indexOfEndOfType = indexOfType;
+while (endOfExtensionNotFound ){
+	indexOfEndOfType += 1;
+	if( (header.charAt(indexOfEndOfType)==13) || (header.substring(indexOfEndOfType,indexOfEndOfType+1)).equals(" ")) {
+	endOfExtensionNotFound = false;
+	}
+}
+extension = header.substring(indexOfType, indexOfEndOfType);
+
+
+
 // make file to write to
-FileOutputStream binWriter = new FileOutputStream("receivedStuff"+this.extension);
+FileOutputStream binWriter = new FileOutputStream("receivedStuff"+ "." + this.extension);
 
 
 // Read data from the server and write it to the screen.
--- ProjectWithCommandAsObject/src/RetrieveDataCommand.java
+++ ProjectWithCommandAsObject/src/RetrieveDataCommand.java
@@ -6,20 +6,11 @@ import java.io.IOException;
 
 public class RetrieveDataCommand extends Command {
 	
-protected String extension;
+protected String header = "";
 	
 	
 public RetrieveDataCommand(String shortHost,String hostExtension, String HTTPVersion, String command,DataOutputStream outToServer,BufferedInputStream inFromServer){
-	super(shortHost,hostExtension,HTTPVersion, command, outToServer, inFromServer);
-	
-	int placeInUrl = hostExtension.length() -1;
-	extension = ".html";
-	while (hostExtension.charAt(placeInUrl) != ("/").charAt(0) && extension == ".html"){
-		if (hostExtension.charAt(placeInUrl) == (".").charAt(0)){
-			extension = hostExtension.substring(placeInUrl);
-		}
-		placeInUrl = placeInUrl -1;
-	}
+	super(shortHost,hostExtension,HTTPVersion, command, outToServer, inFromServer);	
 }
 
 public void execute() throws IOException{
@@ -32,13 +23,12 @@ public void execute() throws IOException{
 
 	// Read data from the server and write it to the screen.
 	int outputFromServer = inFromServer.read();
-	System.out.print((char)outputFromServer);
 	while (outputFromServer != -1 && !entityPartStarted){
 	if (a == 10 && b == 13 && c == 10 && d == 13){
 		entityPartStarted = true;
 	}
 	if (!entityPartStarted){
-		System.out.print((char)outputFromServer);
+		header = header +(char)outputFromServer;
 	}
 	outputFromServer = inFromServer.read();
 	d = c;
@@ -46,6 +36,6 @@ public void execute() throws IOException{
 	b = a;
 	a = outputFromServer;
 	}
-	}
-
+	System.out.print(header);
+}
 }
