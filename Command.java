686a8b0cc1a75c05012fda0a68609b8d9e79dfcd
Matthijs Van keirsbilck <matthijs.vankeirsbilck@student.kuleuven.be>
2015-03-14 00:12:47 +0100

    merging, removing unneeded files

--- Command.java
+++ Command.java
@@ -20,32 +20,32 @@ public abstract class Command {
 		this.inFromServer = inFromServer;
 		createDataToBeSent(command);
 	}
-	
-	
-	
+
+
+
 	public void execute() throws IOException{
 		System.out.println(toBeSent);
 		outToServer.writeBytes(toBeSent);
 	}
-	
-	
-	
-	
+
+
+
+
 	public void createDataToBeSent(String command){
-	if (HTTPVersion.equals("1.0")){
-    	System.out.println("1.0 entered");
-        toBeSent = command + " "  + hostExtension + " HTTP/1.0" + "\r\n\r\n";
-        System.out.println(command +  " "  + hostExtension + " HTTP/1.0" + "\r\n\r\n");
-        }
-    else if (HTTPVersion.equals("1.1")){
-    	System.out.println("1.1 entered");
-    toBeSent = command + " " + hostExtension + " HTTP/1.1" + "\r\n" + "host:" + shortHost + "\r\n\r\n";
-    }
-    else{
-    	//throw error
-    }
+		if (HTTPVersion.equals("1.0")){
+			System.out.println("1.0 entered");
+			toBeSent = command + " "  + hostExtension + " HTTP/1.0" + "\r\n\r\n";
+			System.out.println(command +  " "  + hostExtension + " HTTP/1.0" + "\r\n\r\n");
+		}
+		else if (HTTPVersion.equals("1.1")){
+			System.out.println("1.1 entered");
+			toBeSent = command + " " + hostExtension + " HTTP/1.1" + "\r\n" + "host:" + shortHost + "\r\n\r\n";
+		}
+		else{
+			//throw error
+		}
 	}
-	
-	
-	
+
+
+
 }
