53a82992039630221a2976b9e80c0d9e0c4d8d5c
GertJanEnHarald <spamport@hotmail.com>
2015-03-17 22:31:44 +0100

    put en post op de lelijke manier. Ik vind het ok.

--- Server/PostRespond.java
+++ Server/PostRespond.java
@@ -3,13 +3,30 @@ package Server;
 import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import sun.security.tools.PathList;
 
 public class PostRespond extends ReceiveDataRespond {
 
 	public PostRespond(String[] request, DataOutputStream outToClient,
 			BufferedInputStream inFromClient, int port) throws IOException {
 		super(request, outToClient, inFromClient, port);
-		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void execute() throws IOException{
+		super.execute();
+		String str;
+		try{ Path path = Paths.get("posts.txt");
+		byte[] data = Files.readAllBytes(path);
+		str = new String(data, "UTF-8");
+		}catch( IOException exception){str = "";}
+		PrintWriter out = new PrintWriter("posts.txt");
+		out.print(str);
+		out.print(data);
+		out.close();
+	}
 }
--- Server/ReceiveDataRespond.java
+++ Server/ReceiveDataRespond.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 
 public class ReceiveDataRespond extends Respond {
 
+	protected int length;
+	protected String data = "";
+
 	public ReceiveDataRespond(String[] request, DataOutputStream outToClient,
 			BufferedInputStream inFromClient, int port) throws IOException {
 		super(request, outToClient, inFromClient, port);
@@ -14,8 +17,47 @@ public class ReceiveDataRespond extends Respond {
 
 	@Override
 	public void execute() throws IOException {
-		// TODO Auto-generated method stub
-
+		parseHeader();
+		readData();
+	}
+	
+	protected void readData() throws IOException {
+		int counter = 0;
+		while(counter < length){
+			data += inFromClient.read();
+		}
 	}
 
+	public void parseHeader() {
+		int counter = 1;
+		String header = ""; 
+		while(counter < request.length){
+			if(!request[counter].equals(null)){header += request[counter];}
+		}
+
+
+		//parse for content length
+		int indexOfLength = header.indexOf("Content-Length:");
+		if (indexOfLength >0){
+			boolean lengthNotFound = true;
+			while (lengthNotFound ){
+				if( (header.substring(indexOfLength,indexOfLength+1)).equals(" ")){
+					lengthNotFound = false;
+				}
+				indexOfLength += 1;
+			}
+			boolean endOfLengthNotFound = true;
+			int indexOfEndOfLength = indexOfLength;
+			while (endOfLengthNotFound ){
+				indexOfEndOfLength += 1;
+				if( (header.charAt(indexOfEndOfLength)==13) || (header.substring(indexOfEndOfLength,indexOfEndOfLength+1)).equals(" ")) {
+					endOfLengthNotFound = false;
+				}
+			}
+			length = Integer.parseInt(header.substring(indexOfLength, indexOfEndOfLength));
+		}
+		else{
+			length = -1;
+		}
+	}
 }
